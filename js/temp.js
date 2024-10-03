fa0604e54e143d8c7c006baea4cd74a725fbf04f
2048io-png <justingabrielmreble@gmail.com>
2024-10-03 14:15:58 +0800

    Update temp.js

--- js/temp.js
+++ js/temp.js
@@ -2,6 +2,7 @@ var tmp = {}
 var options = {
     notation: "mixed_sc",
     max_range: 9,
+    mixed_sc: 63,
     pass: 0,
 }
 
@@ -109,4 +110,5 @@ function updateTemp() {
 function updateOptions() {
     options.notation = ['sc','st','mixed_sc','log','elemental'][player.radios["notation"]]
     options.max_range = [3,6,9,12,15][player.radios["comma-format"]]
+    options.mixed.sc = [33,63,303,3003][player.options.mixed_sc]
 }
